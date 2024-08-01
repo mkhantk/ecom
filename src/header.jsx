7ff4a04bed821c7ab5c26db77cbb7448f819dc23
Min Khant Kyaw <mkhantk102@gmail.com>
2024-08-01 14:24:41 +0630

    First Commit

---
+++ src/header.jsx
@@ -0,0 +1,17 @@
+
+const Header = () => {
+
+    return(
+        <>
+            <header>
+                <h1 className="logo"></h1>
+                <input type="search" name="" id="" />
+                <nav>
+                    <li>link1</li>
+                    <li>link2</li>
+                    <li>link3</li>
+                </nav>
+            </header>
+        </>
+    )
+}
