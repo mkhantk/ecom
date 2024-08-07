2d995a4eb63d916efefbe33843cd5009bbc46933
Min Khant Kyaw <mkhantk102@gmail.com>
2024-08-07 17:26:36 +0630

    Add search function

--- src/header.jsx
+++ src/header.jsx
@@ -1,11 +1,24 @@
+import { useState } from "react";
+import { FaSearch } from "react-icons/fa";
 
-const Header = () => {
+const Header = ({ search, setSearch }) => {
+    // const [search, setSearch] = useState('')
+
+    function handleSearch(e) {
+        // console.log(e.target.value)
+        setSearch(e.target.value)
+        console.log(search)
+    }
 
     return(
         <>
             <header className="flex gap-5 justify-end items-center p-5 bg-green-600 bg-opacity-20 mb-5">
                 <h1 className="logo mr-auto text-5xl font-bold">Ecom</h1>
-                <input type="search" name="" id="search" className="ring mr-auto rounded-full px-5 py-1"/>
+                <div className="relative flex justify-center items-center mr-auto">
+                    <input type="search" name="" id="search" className="ring mr-auto rounded-full px-5 py-1 indent-2" onChange={handleSearch}/>  
+                    <FaSearch className="absolute left-2"/>
+                </div>
+                
                 <nav className="list-none flex gap-5">
                     <li>link1</li>
                     <li>link2</li>
