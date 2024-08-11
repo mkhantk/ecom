d9b3537d9e0b6471cafb026f3a7755bb6dd0b6f3
Min Khant Kyaw <mkhantk102@gmail.com>
2024-08-11 15:58:58 +0630

    Add states in the main component

--- src/App.jsx
+++ src/App.jsx
@@ -4,18 +4,33 @@ import viteLogo from '/vite.svg'
 import './App.css'
 import Header from './header'
 import ProdList from './product-list'
+import ProdPage from './product-page'
+import { BrowserRouter as Router, Route, Routes} from 'react-router-dom'
 
 function App() {
   const [search, setSearch] = useState('')
+  const [allData, setAllData] = useState([])
+
 
   return (
     <>
-      <Header search={search} setSearch={setSearch} />
-      <main className='flex gap-5 p-5 max-w-7xl m-auto'>
-        
-        <ProdList search={search}/>
+      <Router>
+        <Header search={search} setSearch={setSearch} />
+        <main className='flex gap-5 p-5 max-w-7xl m-auto'>
+          <Routes>
+            <Route path='/' element={<ProdList search={search} allData={allData} setAllData={setAllData} />} />
+            <Route path='/product-page/:productId' element={<ProdPage allData={allData}  />} />
+          
+          </Routes>
+          
+            
+         
+          
+          
 
-      </main>
+        </main>
+      </Router>
+      
     </>
   )
 }
