968d0bd9627d86904716955a644057bdcde29f39
Min Khant Kyaw <mkhantk102@gmail.com>
2024-08-13 10:47:12 +0630

    Add icons to the header

--- src/header.jsx
+++ src/header.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
-import { FaSearch } from "react-icons/fa";
+import { FaSearch, FaHeart, FaShoppingCart } from "react-icons/fa";
+import { Link } from "react-router-dom";
 
 const Header = ({ search, setSearch }) => {
     
@@ -13,16 +14,22 @@ const Header = ({ search, setSearch }) => {
     return(
         <>
             <header className="flex gap-5 justify-end items-center p-5 bg-green-600 bg-opacity-20 mb-5">
-                <h1 className="logo mr-auto text-5xl font-bold">Ecom</h1>
-                <div className="relative flex justify-center items-center mr-auto">
-                    <input type="search" name="" id="search" className="ring mr-auto rounded-full px-5 py-1 indent-2" onChange={handleSearch}/>  
+                <Link to='/'>
+                    <h1 className="logo mr-auto text-5xl font-bold">Ecom</h1>
+                </Link>
+                <div className="relative flex justify-center items-center m-auto">
+                    <input type="search" name="" id="search" className="mr-auto rounded-full px-5 py-1 indent-2" onChange={handleSearch}/>  
                     <FaSearch className="absolute left-2"/>
                 </div>
                 
                 <nav className="list-none flex gap-5">
-                    <li>link1</li>
-                    <li>link2</li>
-                    <li>link3</li>
+                    {/* <li>link1</li> */}
+                    <li>
+                        <FaHeart />
+                    </li>
+                    <li>
+                        <FaShoppingCart /> 
+                    </li>
                 </nav>
             </header>
         </>
